45896b342cd4273d50665fe8a7c9534abd41821f
Beauclair Ngnintedem <beauclair.dongmo.ngnintedem@cofinpro.de>
2022-01-25 16:20:12 +0100

    CHG: Implement builder pattern for Carsharing

---
+++ Car Sharing/task/src/carsharing/view/CarSharingBuilder.java
@@ -0,0 +1,49 @@
+package carsharing.view;
+
+import carsharing.controller.CarController;
+import carsharing.controller.CompanyController;
+import carsharing.controller.CustomerController;
+import carsharing.repository.BaseRepository;
+import carsharing.repository.CarRepository;
+import carsharing.repository.CompanyRepository;
+import carsharing.repository.CustomerRepository;
+import carsharing.service.CarDaoImpl;
+import carsharing.service.CompanyDaoImpl;
+import carsharing.service.CustomerDaoImpl;
+
+public class CarSharingBuilder {
+
+    private CompanyController companyController;
+    private CarController carController;
+    private CustomerController customerController;
+    private String databaseFilename;
+
+    public CarSharingBuilder init(String databaseFilename) {
+        this.databaseFilename = databaseFilename != null ? databaseFilename : "carSharing";
+        return this;
+    }
+
+    public CarSharingBuilder withDatabase() {
+        BaseRepository.createDatabaseTable(databaseFilename);
+        return this;
+    }
+
+    public CarSharingBuilder withCompanyController() {
+        this.companyController = new CompanyController(new CompanyDaoImpl(CompanyRepository.init(databaseFilename)));
+        return this;
+    }
+
+    public CarSharingBuilder withCarController() {
+        this.carController = new CarController(new CarDaoImpl(CarRepository.init(databaseFilename)));
+        return this;
+    }
+
+    public CarSharingBuilder withCustomerController() {
+        this.customerController = new CustomerController(new CustomerDaoImpl(CustomerRepository.init(databaseFilename)));
+        return this;
+    }
+
+    public CarSharing build() {
+        return new CarSharing(companyController, carController, customerController);
+    }
+}
